d9c3feeb99c68caf71d5eb5ade7b477b38610c1a
Angus McAloon <amcaloon@lcs.on.ca>
2022-01-20 11:30:26 -0500

    Defined data model for the app

---
+++ Shared/Model/Task.swift
@@ -0,0 +1,21 @@
+//
+//  Task.swift
+//  Reminders (iOS)
+//
+//  Created by Angus McAloon on 2022-01-20.
+//
+
+import Foundation
+
+struct Task: Identifiable {
+    var id = UUID()
+    var description: String
+    var priority: TaskPriority
+    var completed: Bool
+}
+
+let testData = [
+    Task(description: "Get long hair", priority: .high, completed: true),
+    Task(description: "Get modelling contract", priority: .medium, completed: false),
+    Task(description: "Retire from teaching", priority: .low, completed: false)
+]
