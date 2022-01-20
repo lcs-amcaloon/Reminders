87b48208e4d075cf19c58d82451cf0783e9062a0
Angus McAloon <amcaloon@lcs.on.ca>
2022-01-20 11:17:33 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Angus McAloon on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
